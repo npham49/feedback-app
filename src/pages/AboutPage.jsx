28a6d1de045a8f80a5ca71466a0fa9e70bdfb6ce
npham49 <nguyenphamswork@gmail.com>
2022-07-17 16:40:37 -0700

    added routes and finished feedback adding

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Card from '../components/share/Card'
+
+
+function AboutPage() {
+  return (
+    <Card>
+        <div className='about'>
+            <h1> About this project</h1>
+            <p>This is a React page to leave reviews</p>
+        </div>
+    </Card>
+  )
+}
+
+export default AboutPage
