ef1e51af59f09d9b2bfcaa5ed749efd47b7a856f
npham49 <nguyenphamswork@gmail.com>
2022-07-28 16:30:00 +0700

    added filed

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 
-import {useState} from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
-import FeedbackData from './data/FeedbackData'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
@@ -11,8 +9,6 @@ import AboutIconLink from './components/AboutIconLink'
 import {FeedbackProvider} from './context/FeedbackContext'
 
 function App() {
-    const [feedback, setFeedback] = useState(FeedbackData)
-    
     
     return (
         <FeedbackProvider>
